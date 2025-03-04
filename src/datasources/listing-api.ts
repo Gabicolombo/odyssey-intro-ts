be61388917ebf3bece864f05bf687545ae43957f
Gabriela Colombo <65355183+Gabicolombo@users.noreply.github.com>
2025-03-04 14:34:13 -0300

    feat: creating mutation logic

--- src/datasources/listing-api.ts
+++ src/datasources/listing-api.ts
@@ -1,5 +1,5 @@
 import { RESTDataSource } from "@apollo/datasource-rest";
-import { Listing, Amenity} from '../types';
+import { Listing, Amenity, CreateListingInput} from '../types';
 
 export class ListingAPI extends RESTDataSource {
   baseURL = "https://rt-airlock-services-listing.herokuapp.com/";
@@ -15,6 +15,12 @@ export class ListingAPI extends RESTDataSource {
 
   getAmenities(listingId: string): Promise<Amenity[]> {
     return this.get<Amenity[]>(`listings/${listingId}/amenities`);
+  }
+
+  createListing(listing: CreateListingInput): Promise<Listing>{
+    return this.post<Listing>("listings", {
+      body: { listing }
+    });
   }
 }
 
